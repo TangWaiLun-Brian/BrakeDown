7d74a766f94e6b08fb8e8967f8131eff579928a8
TangWaiLun-Brian <1155174601@link.cuhk.edu.hk>
2022-12-14 12:25:47 +0800

    test

--- car_racing/envs/tmp_sprite.py
+++ car_racing/envs/tmp_sprite.py
@@ -1,15 +1,16 @@
 import pygame
 from pygame.locals import *
+SCREEN_WIDTH = 450
 class Rectangle(pygame.sprite.Sprite):
-    def __init__(self, center, length, width):
+    def __init__(self, center, width, height):
         super(Rectangle, self).__init__()
         self.center = center
-        self.length = length
         self.width = width
+        self.height = height
         self.color = (255,) * 3
         #print(self.center[0]-self.length//2, self.center[1]+self.width//2, self.center[0]+self.length//2, self.center[1]-self.width//2)
 
-        self.rect = pygame.Rect(self.center[0]-self.length//2, self.center[1]+self.width//2, self.length, self.width)
+        self.rect = pygame.Rect(self.center[0]-self.width//2, self.center[1]+self.height//2, self.width, self.height)
         #print(self.color)
 
 
@@ -17,14 +18,20 @@ class Rectangle(pygame.sprite.Sprite):
         pygame.draw.rect(screen, self.color, self.rect)
 
 class ControlBar(Rectangle):
-    def __init__(self, center, length, width):
-        super(ControlBar, self).__init__(center, length, width)
+    def __init__(self, center, width, height):
+        super(ControlBar, self).__init__(center, width, height)
         self.speed = 1
     def update(self, pressed_keys):
         if pressed_keys[K_LEFT]:
             self.rect.move_ip(-self.speed, 0)
         if pressed_keys[K_RIGHT]:
             self.rect.move_ip(self.speed, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > SCREEN_WIDTH:
+            self.rect.right  = SCREEN_WIDTH
+
 
 if __name__ =='__main__':
     screen = pygame.display.set_mode([450, 800])
