be1b2e9d7ed956b5ec033a823ba65f5e9126069b
Mok Hon Wang <59782174+M-HW@users.noreply.github.com>
2022-12-14 12:01:47 +0800

    draw circle

--- car_racing/envs/__init__.py
+++ car_racing/envs/__init__.py
@@ -3,19 +3,40 @@ from pygame.locals import *
 
 pygame.init()
 
-xdim, ydim = 900, 1600
-screen = pygame.display.set_mode([900, 1600]) 
+clock = pygame.time.Clock()
 
+xdim, ydim = 900, 1600
+screen = pygame.display.set_mode([450, 800]) 
+
+class Ball(pygame.sprite.Sprite):
+
+    def __init__(self, x_cor, y_cor):
+        super().__init__()
+
+        """self.image = pygame.Surface([x_cor, y_cor])
+        self.image.fill((0,0,0))"""
+        self.rect = pygame.draw.circle(screen, (255, 255, 255), (225,400), 3)
+
+        
 
 running = True
+ball_coor = [100,300]
+ball_speed = [0,0]
 while running:
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE: 
                 pygame.quit()
 
-    screen.fill((0, 0, 0))
 
+    
+    ball = Ball(*ball_coor)
+
+    pygame.display.update()
+
+    clock.tick(60)
     pygame.display.flip()
 
 pygame.quit()
