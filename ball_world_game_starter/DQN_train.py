ba541f555f22bd42538103a585da0ae179cf7cb7
TangWaiLun-Brian <1155174601@link.cuhk.edu.hk>
2022-12-26 01:08:29 +0800

    Upload current best result

--- ball_world_game_starter/DQN_train.py
+++ ball_world_game_starter/DQN_train.py
@@ -133,7 +133,7 @@ def main(env):
     window_size = 15
     reward_sma = [sum(reward_list[i:i+window_size]) / window_size for i in range(0, len(reward_list)-window_size+1)]
     ax1.plot(np.arange(len(reward_sma)), reward_sma)
-    ax1.set_title('Total Reward over Episodes')
+    ax1.set_title('SMA Reward over Episodes')
 
     fig1.savefig('./experimental results/Reward_Graph.png')
 
