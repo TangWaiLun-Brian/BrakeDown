a9c4aa14bbcf1eef79102ea2fc95a37722fe713b
TangWaiLun-Brian <1155174601@link.cuhk.edu.hk>
2022-12-26 21:22:10 +0800

    Refractor code

--- ball_world_game_starter/Testing_config.py
+++ ball_world_game_starter/Testing_config.py
@@ -6,9 +6,9 @@ parser.add_argument('--seed', type=int, default=1)
 parser.add_argument('--num_of_obs', type=int, default=10)
 parser.add_argument('--num_of_br', type=int, default=5)
 parser.add_argument('--num_of_acc', type=int, default=1)
-parser.add_argument('--ball_initial_speed', type=float, default=6.)
+parser.add_argument('--ball_initial_speed', type=float, default=3.)
 
-parser.add_argument('--fps', type=int, default=24)
+parser.add_argument('--fps', type=int, default=120)
 parser.add_argument('--mode', type=str, choices=['test'], default='test')
 parser.add_argument('-v', action='store_true')
 # Our current model is 'my_dqn_weight_2.h5'
