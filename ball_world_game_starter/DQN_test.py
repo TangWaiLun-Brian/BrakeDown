dd7f512affedb0199959992fd467a48470f72774
TangWaiLun-Brian <1155174601@link.cuhk.edu.hk>
2022-12-26 16:37:37 +0800

    Refractor code

--- ball_world_game_starter/DQN_test.py
+++ ball_world_game_starter/DQN_test.py
@@ -6,34 +6,42 @@ import ball_world_game
 from Testing_config import args
 
 def test(env):
+    # construct model
     model = build_model(env.observation_space.sample().shape[0], env.action_space.n)
+    # load model (including raise exception framework)
     try:
         model.load_weights(args.model_name)
     except:
         print(f'Error occured when loading {args.model_name}')
         exit()
+    # initialize lists and random seed
     collect_list = []
     reward_list = []
     success_list = []
     random_seed = args.seed
     np.random.seed(random_seed)
     for i in range(args.episode):
+        # reset environment and total reward
         total_testing_reward = 0
         observation = env.reset(seed=random_seed)
-        render = True
         done = False
         while not done:
+            # model takes a move
             predicted = model.predict(observation.reshape(1, -1)).reshape(-1)
             action = np.argmax(predicted)
             new_observation, reward, done, info = env.step(action)
 
             total_testing_reward += reward
             observation = new_observation
+        # print info after every episode
         print(f'testing reward: {total_testing_reward: 5} collected brakes: {env.ball.count}')
+
         collect_list.append(env.ball.count)
         reward_list.append(total_testing_reward)
         success_list.append(env.ball.count >= 5)
         random_seed = np.random.randint(0, 100000)
+
+    # print final result
     print(f'Success Rate: {sum(success_list) / len(success_list): 5} Average Reward: {sum(reward_list) / len(reward_list): 6} Average Collect: {sum(collect_list) / len(collect_list)}')
 
 
--- ball_world_game_starter/ball_world_game_play.py
+++ ball_world_game_starter/ball_world_game_play.py
@@ -7,9 +7,10 @@ from Play_config import args
 import ball_world_game
 
 def main(env):
+    # assign frame rate
     if args.fps > 0:
         env.metadata['render_fps'] = args.fps
-
+    # assign random seed for Random Agent
     env.action_space.seed(args.seed)
 
     collect_list = []
@@ -18,10 +19,8 @@ def main(env):
     random_seed = args.seed
     np.random.seed(random_seed)
 
-
-
+    # Loop over a certain number of games
     for i in range(args.episode):
-        total_testing_reward = 0
         observation = env.reset(seed=random_seed)
 
         if args.mode == 'human_rand':
@@ -30,9 +29,8 @@ def main(env):
         render = True
         done = False
         while not done:
-            # if render:
-            #    env.render()
             if args.mode == 'human':
+                # collect user input through keyboard
                 pressed_keys = pygame.key.get_pressed()
                 action = 1
                 if pressed_keys[K_LEFT]:
@@ -40,6 +38,7 @@ def main(env):
                 if pressed_keys[K_RIGHT]:
                     action = 2
             else:
+                # sample a random action from agent
                 action = env.action_space.sample()
 
             new_observation, reward, done, info = env.step(action)
@@ -48,20 +47,23 @@ def main(env):
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         env.close()
-                        # pygame.quit()
 
+        # print info after finishing every episode
         print(f'Episode {i} collected brakes: {env.ball.count}')
+
         collect_list.append(env.ball.count)
         success_list.append(env.ball.count >= 5)
         random_seed = np.random.randint(0, 100000)
-
+    # print final result
     print(f'Success Rate: {sum(success_list) / len(success_list): .2f} Average Collect: {sum(collect_list) / len(collect_list)}')
 
 
 if __name__ == '__main__':
+    # adjust mode to be visible or not
     mode = args.mode
     if mode == 'human_rand':
         mode = 'test' if args.v else 'train'
+
     env = gym.make('ball_world_game/env_main-v0', render_mode=mode, num_of_obs=args.num_of_obs,
                    num_of_br=args.num_of_br, num_of_acc=args.num_of_acc, ball_initial_speed=args.ball_initial_speed)
     main(env)
