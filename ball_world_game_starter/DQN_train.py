46a44c804f2865e7d9c3e5aa9e06621174eebbf3
TangWaiLun-Brian <1155174601@link.cuhk.edu.hk>
2022-12-26 11:38:23 +0800

    Refractor train code

--- ball_world_game_starter/DQN_train.py
+++ ball_world_game_starter/DQN_train.py
@@ -7,39 +7,48 @@ import matplotlib.pyplot as plt
 from Training_config import args
 
 
-def train(env, replay_memory, model, target_model, done):
+def train(replay_memory, model, target_model):
     learning_rate = args.learning_rate
     discount_factor = args.discount_factor
     MIN_REPLAY_SIZE = 1000
     batch_size = args.batch_size
 
-    if not replay_memory.can_provide_sample(MIN_REPLAY_SIZE):
+    # We won't carry out training if we dont have enough samples, to avoid over-fitting
+    if not replay_memory.have_sample(MIN_REPLAY_SIZE):
         return
-
+    # Sample a batch of states from replay memory
     mini_batch = replay_memory.sample(batch_size)
     current_states = np.array([transition[0] for transition in mini_batch])
+    # make prediction on current states with main model
     current_qs_list = model.predict(current_states)
     new_current_states = np.array([transition[3] for transition in mini_batch])
+    # make prediction on next states with target model
     future_qs_list = target_model.predict(new_current_states)
 
     X = []
     Y = []
     for index, (observation, action, reward, new_observation, done) in enumerate(mini_batch):
         if not done:
+            # try to maximize future q value with respect to the target model prediction on next state
             max_future_q = reward + discount_factor * np.max(future_qs_list[index])
         else:
+            # there is no next state as the game is terminated
             max_future_q = reward
-
+        # try to update the current q value using Bellman Equation
         current_qs = current_qs_list[index]
         current_qs[action] = (1 - learning_rate) * current_qs[action] + learning_rate * max_future_q
 
         X.append(observation)
         Y.append(current_qs)
+    # Train our model to have X as input and output Y
     model.fit(np.array(X), np.array(Y), batch_size=batch_size, verbose=0, shuffle=True)
 
 
 class ReplayMemory():
-
+    """
+    Each of the experience in Our Replay Memory consists of
+    observation, action, reward, new observation and terminate flag
+    """
     def __init__(self, capacity):
         self.capacity = capacity
         self.memory = []
@@ -57,23 +66,26 @@ class ReplayMemory():
         samples = random.sample(self.memory, batch_size)
         return samples
 
-    def can_provide_sample(self, batch_size):
+    def have_sample(self, batch_size):
         return len(self.memory) >= batch_size
 
 
 def main(env):
     max_episodes = args.episode
-
-    max_epsilon = args.max_epsilon  # You can't explore more than 100% of the time
-    min_epsilon = args.min_epsilon  # At a minimum, we'll always explore 1% of the time
-    epsilon = max_epsilon  # Epsilon-greedy algorithm in initialized at 1 meaning every step is random at the start
+    # Epsilon-greedy Algorithm's parameter
+    max_epsilon = args.max_epsilon
+    min_epsilon = args.min_epsilon
+    epsilon = max_epsilon
 
     decay = args.decay_rate
 
-    render = True
+    train_step = 4
+    update_step = 100
+    # Main model
     model = build_model(env.observation_space.sample().shape[0], env.action_space.n)
     # Target model, update every 100 steps
     target_model = build_model(env.observation_space.sample().shape[0], env.action_space.n)
+    # Initialize main, target model weight
     target_model.set_weights(model.get_weights())
 
     replay_memory = ReplayMemory(args.replay_size)
@@ -89,28 +101,29 @@ def main(env):
 
         while not done:
             steps += 1
-
+            # Gen a random number, else-part executes more when epsilon decreases with episode
             rand_num = np.random.rand()
-
             if rand_num <= epsilon:
                 action = env.action_space.sample()
             else:
                 predicted = model.predict(observation.reshape(1, -1)).reshape(-1)
                 action = np.argmax(predicted)
 
+            # Take a move and get the next observation, reward and terminate flag
             new_observation, reward, terminated, info = env.step(action)
             replay_memory.push([observation, action, reward, new_observation, terminated])
-
-            if steps % 4 == 0 or terminated:
-                train(env, replay_memory, model, target_model, terminated)
+            # Train the model for every [train_step] steps or the game is terminated (either win or lose)
+            if terminated or steps % train_step == 0:
+                train(replay_memory, model, target_model)
 
             observation = new_observation
             total_training_reward += reward
 
             if terminated:
+                # Print training information after finishing each episode
                 print(f'Episode {episode} Total Reward: {total_training_reward:7} Final Reward: {reward:7}')
-
-                if steps >= 100:
+                # After some steps, update the main model to the target model
+                if steps >= update_step:
                     print('Copy model weight to target model....')
                     target_model.set_weights(model.get_weights())
 
@@ -119,7 +132,7 @@ def main(env):
 
         reward_list.append(total_training_reward)
         epsilon_list.append(epsilon)
-
+        # Decay epsilon according to the greedy algorithm
         epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay * episode)
 
     print('Finish Training. Saving Model...')
