64230efc30c68f2f60c7baa753034adb3a696272
TangWaiLun-Brian <1155174601@link.cuhk.edu.hk>
2022-12-26 18:29:37 +0800

    Refractor code

--- ball_world_game_starter/AIplayer.py
+++ ball_world_game_starter/AIplayer.py
@@ -9,6 +9,10 @@ import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
 from keras.optimizers import Adam
+# not sure if the import within calling any agent or method is valid but it does speed up training
+speed_up = False
+if speed_up:
+    import rl.agents
 
 def build_model(states, actions):            # pass states from the envirnment and action into the model
     model = Sequential()
@@ -17,5 +21,7 @@ def build_model(states, actions):            # pass states from the envirnment a
     model.add(Dense(24, activation='relu', name="Hidden_layer_2"))
     model.add(Dense(actions, activation='linear', name="Output_layer"))  # last layer output the actions
     model.compile(loss=tf.keras.losses.mae, optimizer=Adam(lr=0.01), metrics=['mae'])
+    print(model.summary())
+
     return model
 
